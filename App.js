75beae40231b48a6de162e10691767d2ddba7361
worldkaizen001 <aigbekelvin@gmail.com>
2022-04-20 09:40:38 +0800

    first

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, Button,Linking, SafeAreaView} from 'react-native';
+import tw from 'tailwind-react-native-classnames';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {styles.body}>
+      <Text style = {styles.text}> EverGreen Meals</Text>
+      <Text style = {styles.texttwo}> Eat Well</Text>
+      <View>
+        
+      </View>
+      <View style={styles.row}>
+        <Text> Today </Text>
+      <Text> good day</Text>
+      </View>
+      <Text style = {styles.texttwo}> Pro Mode</Text>
+      <Button title='Try' onPress={()=>{}}></Button>
+      <Button title='youtube chaneel' onPress={()=>{Linking.openURL('https://www.youtube.com/results?search_query=dotted+container+flutter')}}></Button>
     </View>
   );
 }
 
+// const styles = StyleSheet.Text({
+//   Text:{
+//     color: '#fff',
+//     fontSize: 20,
+    
+//   }
+// })
+
 const styles = StyleSheet.create({
-  container: {
+  body: {
+    
+    backgroundColor: 'white',
+    alignItems: 'flex-start',
+    
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
+    
   },
+  row: {
+    
+    backgroundColor: "white",
+    marginTop: 20,
+    alignItems: "center",
+    alignContent: "center",
+    flexDirection: "column"
+    
+  },
+  text:{
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    fontSize: 30,
+    marginTop:40,
+    marginHorizontal: 13,
+    paddingBottom: 10,
+    
+    color: 'black'
+  },
+  texttwo:{
+    marginHorizontal: 13,
+    fontStyle: 'italic',
+    fontSize: 25,
+    
+   
+    color: 'black',
+    fontWeight: 'bold'
+  },
+  button:{
+    color: '#ffffff'
+  }
 });
---
+++ src/components/header.js
@@ -0,0 +1,33 @@
+import {View,StyleSheet} from "react-native"
+
+export default function Header(){
+    return(
+        <View style ={styles.container}>
+            <Text style={styles.largeText}>grab your</Text>
+            <Text style = {styles.smallText}>delicacies</Text>
+        </View>
+
+    )
+}
+
+const styles = StyleSheet.create({
+    largeText: {
+        fontSize: 30,
+        fontStyle: "normal",
+        fontWeight: "bold",
+        color: "black"
+    },
+    smallText: {
+        fontSize: 23,
+        fontStyle: "normal",
+        fontWeight: "600",
+        color: "black"
+    },
+
+    container: {
+        backgroundColor: "white",
+        marginHorizontal: 15,
+        
+    },
+
+})
